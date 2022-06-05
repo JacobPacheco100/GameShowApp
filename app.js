b786ac89c07cf3cc92228ecf9acd0e7438c3a580
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-06-05 17:21:25 -0400

    random phrase genorator, split phrase, fill phrase

--- app.js
+++ app.js
@@ -12,24 +12,35 @@ start.addEventListener('click', (e) => {
 })
 
 const phraseList = {
-    0: "monkey see",
+    0: "i will survive",
     1: "stand by me",
     2: "lift off",
     3: "cat in the hat",
+    4: "ready or not",
+    5: "fight club",
+    6: "john cena",
+    7: "doctor strange",
+}
+
+function randomPhrase() {
+    let size = Object.keys(phraseList).length;
+    return Math.floor(Math.random() * size)
 }
 
 function splitPhrase(i) {
     return phraseList[i].split('')
 }
 
-let currentPhrase = splitPhrase(0)
+let currentPhrase = splitPhrase(randomPhrase())
 
+// fill phrase
 for (i = 0; i < currentPhrase.length; i++) {
     const newLi = document.createElement('li')
     newLi.className += 'letter'
     newLi.textContent = currentPhrase[i]
+
     if (currentPhrase[i] === ' ') {
-        currentPhrase[i].style.background = 'white'
+        newLi.style.background = 'white'
     }
 
     phraseUl.appendChild(newLi)
