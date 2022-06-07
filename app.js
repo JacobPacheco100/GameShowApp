6a594a02400c2edb89ef65cf82d9ee4246e24ef6
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-06-07 13:17:40 -0400

    RESTART

--- app.js
+++ app.js
@@ -1,80 +1,36 @@
-const qwerty = document.getElementById('qwerty')
-const phrase = document.getElementById('phrase')
-const phraseUl = document.querySelector('.phraseUl')
-const overlay = document.getElementById('overlay')
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const overlay = document.querySelector('#overlay');
+const startButton = document.querySelector('.btn__reset');
+const phraseUL = document.querySelector('#phrase ul');
+const keyboard = document.querySelector('#qwerty');
+const heartList = document.querySelector('#scoreboard ol');
+const heartArray = document.querySelectorAll('.tries img');
+const heart = document.querySelector('.tries img');
 
+let missed = 0;
+let phrases = ['heart of gold',
+			   'love is blind',
+			   'cool as a cucumber',
+			   'bring home the bacon',
+			   'saved by the bell',
+			   'dressed to the nines'];
 
-const startButton = document.querySelector('.btn__reset')
 startButton.addEventListener('click', () => {
-    overlay.style.display = 'none'
-})
-
-let missed = 0
-
-const phraseList = [
-    "i will survive",
-    "stand by me",
-    "lift off",
-    "cat in the hat",
-    "ready or not",
-    "fight club",
-    "john cena",
-    "doctor strange",
-]
-let currentPhrase
+	if (startButton.textContent === 'Start Game') {
+		startGame();
+		overlay.style.display = 'none';	
+	} else {
+		resetGame();
+		startGame();
+		overlay.style.display = 'none';
+	}
+});
 
 const getRandomPhrasesAsArray = arr => {    // phraseList
-    function random(arr) {
-        return Math.floor(Math.random() * arr.length)
-    }
-    currentPhrase = phraseList[random(phraseList)].split('')
-    return currentPhrase
-}
-const addPhraseToDisplay = arr => { 
-    for (i = 0; i < arr.length; i++) {
-        const newLi = document.createElement('li')
-        if (arr[i] === ' ') {
-            newLi.className += 'space'
-            phraseUl.appendChild(newLi)
-        } else {
-            newLi.className += 'letter'
-            newLi.textContent = arr[i]
-            phraseUl.appendChild(newLi)
-        }
-    }
-}
-addPhraseToDisplay(getRandomPhrasesAsArray(phraseList))
-
-const checkLetter = button => {
-    let match 
-    const lis = document.getElementsByTagName('li')
-    for (i = 0; i < currentPhrase.length; i++) {
-        if (button === lis[i].textContent) {
-            lis[i].className += 'show'
-            match = true
-        } else {
-            match = false
-        }
-    }
-    return match
+    const random = Math.floor(Math.random() * arr.length)
+    const randomPhrase = arr[random]
+    const characters = randomPhrase.split('')
+    return characters
 }
 
-qwerty.addEventListener('click', e => {
-    const btn = e.target
-    const btnLetter = btn.textContent
-    if (btn.tagName === 'BUTTON') {
-        btn.className += 'chosen'
-        let check = checkLetter(btnLetter)
-        if (check === false) {
-            missed++
-            console.log(missed); // this is where im at!
-            btn.style.background = '#ff931f'
-        } else {
-            btn.style.background = ''
-        }
-    }
-})
-
-const checkWin = () => {
-    
-}
